749b28e3182415940062832d5fecb57fc68d0ec3
JonOuellette <jonathan.t.ouellette@gmail.com>
2023-09-24 15:41:28 -0700

    build: Create User model in order for users to register to the site and validations for user authentication.

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
+
 from models import connect_db, db, User
+
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/flask-feedback'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO']=True
+app.config['SECRET_KEY'] =  "youcantseeme"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+connect_db(app)
+db.create_all()
+
+toolbar = DebugToolbarExtension(app)
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
+bcrypt=Bcrypt()
 db=SQLAlchemy()
 
 def connect_db(app):
@@ -7,5 +9,45 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
-class Users(db.Model):
+class User(db.Model):
+    """Sites user"""
+
+    __tablename__ = "users"
+
+    username = db.Column(db.String, primary_key = True, unique = True)
+    password = db.Column(db.Text, nullable = False)
+    email = db.Column(db.String, nullable = False)
+    first_name = db.Column(db.Text, nullable = False)
+    last_name = db.Column(db.Text, nullable = False)
+
+    @classmethod
+    def register(cls, username, password, first_name, last_name, email):
+        """registers user with a hashed password and returns user"""
+        
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode("utf8")
+
+        user = cls(
+            username=username,
+            password=hashed_utf8,
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+
+        db.session.add(user)   
+
+        return user 
     
+    @classmethod
+    def authenticate(cls, username, password,):
+        """Validates that the user exists and that the password is correct.  
+        Returns user if valid if not returns false"""
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            
+            return user
+        else: 
+            return False
