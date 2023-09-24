5669eccfaa2b0096d590e4fa3592614ee6c31490
JonOuellette <jonathan.t.ouellette@gmail.com>
2023-09-24 16:06:32 -0700

    build: Create base html form which includes nav bar and and flash message sections that will extend to other templates which will display to users on the apps pages.

--- app.py
+++ app.py
@@ -8,11 +8,10 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/flask-feedback'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO']=True
-app.config['SECRET_KEY'] =  "youcantseeme"
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "shhhhh"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-connect_db(app)
-db.create_all()
-
 toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
--- forms.py
+++ forms.py
@@ -1,5 +1,4 @@
 """forms for flask app"""
-
+from flask_wtf import FlaskForm
 from wtforms import SearchField, PasswordField
 from wtforms.validators import InputRequired, Length, Email
-from flask_wtf import FlaskForm
