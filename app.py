9ef3d1a8b7f774691f08bf70ae0e1832a1266ce7
JonOuellette <jonathan.t.ouellette@gmail.com>
2023-09-24 16:49:05 -0700

    build: Initial routes completed to allow users to go from the home page of the app to register to the page.  feat: client side home page and user form completed to allow for sign up to the site.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import connect_db, db, User
+from forms import RegisterForm, LoginForm
 
 
 app = Flask(__name__)
@@ -15,3 +16,26 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
+
+@app.route("/")
+def homepage():
+    return redirect("/users/register")
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register_user():
+    
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.username.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+    
+        new_user = User.register(username, password, first_name, last_name, email)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect('/secret')
--- forms.py
+++ forms.py
@@ -1,18 +1,18 @@
 """forms for flask app"""
 from flask_wtf import FlaskForm
-from wtforms import Stringfield, PasswordField
+from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Length, Email
 
 class LoginForm(FlaskForm):
     """User login form"""
 
-    username = Stringfield("Username", validators = [InputRequired(), Length(min=1, max=25)])
+    username = StringField("Username", validators = [InputRequired(), Length(min=1, max=25)])
     password = PasswordField("Password", validators=[InputRequired(), Length(min=6, max =20)])
 
 class RegisterForm(FlaskForm):
 
-    username = Stringfield("Username", validators = [InputRequired(), Length(min=1, max=25)])
+    username = StringField("Username", validators = [InputRequired(), Length(min=1, max=25)])
     password = PasswordField("Password", validators=[InputRequired(), Length(min=6, max =20)])
-    email = Stringfield("Email", validators= [InputRequired(), Email(),])
-    first_name = Stringfield("First Name", validators=[InputRequired(), Length(max=30)])
-    last_name = Stringfield("Last Name", validators=[InputRequired(), Length(max=30)])
+    email = StringField("Email", validators= [InputRequired(), Email(),])
+    first_name = StringField("First Name", validators=[InputRequired(), Length(max=30)])
+    last_name = StringField("Last Name", validators=[InputRequired(), Length(max=30)])
