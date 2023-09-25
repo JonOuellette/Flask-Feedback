fa424aad93c7a446640f95de5620c9fe8e800b62
JonOuellette <jonathan.t.ouellette@gmail.com>
2023-09-24 17:43:52 -0700

    fix: issues with authenticating through the login route - corrected error with form password data being submitted.

--- app.py
+++ app.py
@@ -47,4 +47,22 @@ def register_user():
 def secret_page():
     return render_template("secret.html")
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    """Generates login form to handle logins"""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        user = User.authenticate(username, password)
+
+        if user:
+            return redirect("/secret")
+        
+        else:
+            form.username.errors = ["invalid username/password."]
+            return redirect("/login")
     
+    return render_template("users/login.html", form=form)
